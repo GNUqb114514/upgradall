2f584e4efbb7b1c2b9e25294b1fab9fe86b4a0a2
QB <GNUqb114514@outlook.com>
2024-07-20 11:26:23 +0800

    feat: Make all stuff optional

--- upgradeall.sh
+++ upgradeall.sh
@@ -1,4 +1,8 @@
-echo "Upgrading Arch packages and AURs..."
-paru -Syu # Upgrade packages from AURs
-echo "Upgrading flatpaks..."
-flatpak update # Update packages from flatpak
+if command -v paru ; then
+	echo "Upgrading Arch packages and AURs..."
+	paru -Syu # Upgrade packages from AURs
+fi
+if command -v flatpak ; then
+	echo "Upgrading flatpaks..."
+	flatpak update # Update packages from flatpak
+fi
