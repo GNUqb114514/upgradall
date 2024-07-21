86897e7136abbb2127c41fbbca04199def4ffc5a
QB <GNUqb114514@outlook.com>
2024-07-21 18:51:42 +0800

    fix: Fix the bug that vim-plug upgrading don't work

--- upgradeall.sh
+++ upgradeall.sh
@@ -1,12 +1,12 @@
-if [-f '~/.vim/autoload/plug.vim'] ; then
+if [ -f ~/.vim/autoload/plug.vim ] ; then
 	echo "Upgrading Vim plugins..."
 	vim +PlugUpdate +plugUpgrade +qa
 fi
-if command -v paru ; then
+if command -v paru > /dev/null ; then
 	echo "Upgrading Arch packages and AURs..."
 	paru -Syu # Upgrade packages from AURs
 fi
-if command -v flatpak ; then
+if command -v flatpak > /dev/null ; then
 	echo "Upgrading flatpaks..."
 	flatpak update # Update packages from flatpak
 fi
